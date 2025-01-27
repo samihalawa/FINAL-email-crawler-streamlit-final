d6b3de9b18bed780e0683b8547d43349d1de8f00
Sami <eugproductions@gmail.com>
2025-01-27 16:30:27 +0100

    borrado todo y empezamos con googlesearchapi as the new system and protection is harder to elude. 100 search queries per day each 10 results

--- archive/tools/create_tables.py
+++
@@ -1,9 +0,0 @@
-from models import Base
-from sqlalchemy import create_engine
-
-engine = create_engine("postgresql+psycopg2://postgres.whwiyccyyfltobvqxiib:SamiHalawa1996@aws-0-eu-central-1.pooler.supabase.com:6543/postgres")
-
-# Create all tables
-Base.metadata.create_all(engine)
-
-print("Database tables created successfully!")
